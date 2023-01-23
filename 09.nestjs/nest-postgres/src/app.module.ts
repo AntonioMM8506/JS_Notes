ffbe01d5eae65aede6268d5c9f76fcdd9244299e
edyerena086 <edyerena086@gmail.com>
2023-01-23 12:24:25 -0600

    Added monorepo

--- 09.nestjs/nest-postgres/src/app.module.ts
+++ 09.nestjs/nest-postgres/src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // Import db dependencies
-import { TypeOrmModule } from '@nestjs/typeorm';
+//import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+    /*TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'postgres-db-test',
       port: 5432,
@@ -16,7 +19,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'tododb',
       entities: [],
       synchronize: true,
-    }),
+    }),*/
+    UsersModule,
+    AuthModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- 09.nestjs/nest-postgres/src/main.ts
+++ 09.nestjs/nest-postgres/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Lets config swagger doc and build it
+  const config = new DocumentBuilder()
+    .setTitle('Example of API docs')
+    .setDescription('This is the descript of the swagger doc')
+    .setVersion('2.0')
+    .addTag('restAPI')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
